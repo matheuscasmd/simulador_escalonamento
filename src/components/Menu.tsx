1f0bca164321eff7baa11936ca097e874f8a2df4
Matheus Dias <matheuscasmd@gmail.com>
2025-02-01 10:14:54 -0300

    :wrench: refactor: configurado redirecionamento do menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,19 +17,21 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { useNavigate } from 'react-router-dom'
 
 const menuItems = [
-  { icon: Home, label: "Home", tooltip: "Página inicial" },
-  { icon: CircleHelp, label: "Ajuda", tooltip: "Acessar a documentação"},
-  { icon: RotateCcw, label: "Reiniciar entradas", tooltip: "Reiniciar a entrada de dados" },
-  { icon: History, label: "Histórico de execução", tooltip: "Ver histórico de execuções" },
+  { icon: Home, label: "Home", tooltip: "Página inicial", url: "/home" },
+  { icon: CircleHelp, label: "Ajuda", tooltip: "Acessar a documentação", url: "/app/ajuda"},
+  { icon: RotateCcw, label: "Reiniciar entradas", tooltip: "Reiniciar a entrada de dados", url: "/app/processos" },
+  { icon: History, label: "Histórico de execução", tooltip: "Ver histórico de execuções", url: "/app/historico" },
 ]
 
 export function Menu() {
+  const router = useNavigate()
   return (
     <SidebarProvider className='w-fit pt-4 text-white '>
       <Sidebar className='max-w-21 border-none bg-white'>
-        <SidebarHeader className='w-full flex flex-row items-center text-left justify-start bg-sidebarItem'>
+        <SidebarHeader className='w-full flex flex-row items-center text-left justify-start bg-sidebarItem cursor-pointer' onClick={()=>router("/home")}>
           <img src="src\assets\Group.png" alt="logo" className='w-16'/>
           <span className="text-5xl font-semibold tracking-tight">SO<span className='text-lg'>Simulator</span></span>
         </SidebarHeader>
@@ -40,7 +42,7 @@ export function Menu() {
                 <SidebarMenuItem key={index} className='bg-sidebarItem'>
                   <Tooltip>
                     <TooltipTrigger asChild >
-                      <SidebarMenuButton className='text-lg'>
+                      <SidebarMenuButton className='text-lg' onClick={() => router(item.url)}>
                         <item.icon className="mr-2 size=- stroke-green-600" />
                         {item.label}
                       </SidebarMenuButton>
